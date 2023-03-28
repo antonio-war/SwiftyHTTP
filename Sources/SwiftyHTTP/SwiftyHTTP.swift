2a6f0cad9d0922e2a41b3145b010d643e1490ec2
Antonio War <antonioguerra.it@gmail.com>
2023-03-28 17:28:14 +0200

    feat: add simple log test: add log tests

--- Sources/SwiftyHTTP/SwiftyHTTP.swift
+++ Sources/SwiftyHTTP/SwiftyHTTP.swift
@@ -2,9 +2,16 @@ import Foundation
 
 public struct SwiftyHTTP {
     private init() {}
-        
-    public static func request(with representable: URLRequestRepresentable, completion: @escaping (Result<SwiftyHTTPResponse<Data>, Error>) -> ()) {
+    
+    public static func request(with representable: URLRequestRepresentable, log: Bool = false, completion: @escaping (Result<SwiftyHTTPResponse<Data>, Error>) -> ()) {
         do {
+            if log, let body = try? representable().httpBody {
+                print("------------------------------------------------------------------------")
+                print("SwiftyHTTPRequest.body:\n")
+                print(String(decoding: body, as: UTF8.self))
+                print("------------------------------------------------------------------------")
+            }
+            
             URLSession.shared.dataTask(with: try representable()) { data, response, error in
                 guard let data, let response = response as? HTTPURLResponse else {
                     if let error {
@@ -14,6 +21,14 @@ public struct SwiftyHTTP {
                     }
                     return
                 }
+                
+                if log {
+                    print("------------------------------------------------------------------------")
+                    print("SwiftyHTTPResponse.body:\n")
+                    print(String(decoding: data, as: UTF8.self))
+                    print("------------------------------------------------------------------------")
+                }
+                
                 completion(.success(SwiftyHTTPResponse(urlResponse: response, body: data)))
             }.resume()
         } catch {
@@ -22,7 +37,7 @@ public struct SwiftyHTTP {
     }
     
     @available(iOS 13.0.0, *)
-    public static func request(with representable: URLRequestRepresentable) async -> Result<SwiftyHTTPResponse<Data>, Error> {
+    public static func request(with representable: URLRequestRepresentable, log: Bool = false) async -> Result<SwiftyHTTPResponse<Data>, Error> {
         return await withCheckedContinuation { continuation in
             request(with: representable) { result in
                 continuation.resume(returning: result)
@@ -30,7 +45,7 @@ public struct SwiftyHTTP {
         }
     }
     
-    public static func request<Body: SwiftyHTTPBody>(with representable: URLRequestRepresentable, body: Body.Type, completion: @escaping (Result<SwiftyHTTPResponse<Body>, Error>) -> ()) {
+    public static func request<Body: SwiftyHTTPBody>(with representable: URLRequestRepresentable, body: Body.Type, log: Bool = false, completion: @escaping (Result<SwiftyHTTPResponse<Body>, Error>) -> ()) {
         request(with: representable) { result in
             switch result {
             case .success(let response):
@@ -47,7 +62,7 @@ public struct SwiftyHTTP {
     }
     
     @available(iOS 13.0.0, *)
-    public static func request<Body: SwiftyHTTPBody>(with representable: URLRequestRepresentable, body: Body.Type) async -> Result<SwiftyHTTPResponse<Body>, Error> {
+    public static func request<Body: SwiftyHTTPBody>(with representable: URLRequestRepresentable, body: Body.Type, log: Bool = false) async -> Result<SwiftyHTTPResponse<Body>, Error> {
         return await withCheckedContinuation { continuation in
             request(with: representable, body: body) { result in
                 continuation.resume(returning: result)
