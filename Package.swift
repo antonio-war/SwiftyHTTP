8776bf6be4526597602b03f7d9860db49f797957
Antonio War <antonioguerra.it@gmail.com>
2023-05-19 17:55:50 +0200

    build: update SwiftyHTTP

--- Package.swift
+++ Package.swift
@@ -13,15 +13,16 @@ let package = Package(
             targets: ["SwiftyHTTP"]),
     ],
     dependencies: [
-        // Dependencies declare other packages that this package depends on.
-        // .package(url: /* package url */, from: "1.0.0"),
+        .package(url: "https://github.com/antonio-war/SwiftyCache", branch: "develop")
     ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
         // Targets can depend on other targets in this package, and on products in packages this package depends on.
         .target(
             name: "SwiftyHTTP",
-            dependencies: []),
+            dependencies: [
+                "SwiftyCache"
+            ]),
         .testTarget(
             name: "SwiftyHTTPTests",
             dependencies: ["SwiftyHTTP"]),
